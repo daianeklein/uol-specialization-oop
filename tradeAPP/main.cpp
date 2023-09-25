1d4f77840db8931fa34ddfe8768552cc840a7b0f
daianekoya <daiane.klein22@gmail.com>
2023-09-25 13:33:05 -0300

    refactoring the code into functions

--- tradeAPP/main.cpp
+++ tradeAPP/main.cpp
@@ -1,70 +1,87 @@
 #include <iostream>
 
+void printMenu()
+{
+    // 1 print help
+    std::cout << "1: Print Help " << std::endl;
+
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats " << std::endl;
+    
+    // 3 make an offer
+    std::cout << "3: Make an ask " << std::endl;
+    
+    // 4 make a bid
+    std::cout << "4: Make a bid" << std::endl;
+    
+    // 5 print wallet
+    std::cout << "5: Print Wallet" << std::endl;
+    
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
+    std::cout << "Type in 1 - 6" << std::endl;
+
+
+}
+
+int getUserOption()
+{
+    int userOption;
+    std::cout << "==============================" << std::endl;
+    std::cin >> userOption;
+    std::cout << "You chose " << userOption << std::endl;
+    return userOption;
+
+}
+
+void processUserOption(int userOption)
+{
+     if (userOption == 0)
+     {
+         std::cout << "Invalid Choice. Choose 1-6" << std::endl;
+     }
+
+     if (userOption == 1)
+     {
+         std::cout << "Your aim is to make money, analyse the market and make bids" << std::endl;
+     }
+
+     if (userOption == 2)
+     {
+         std::cout << "Market looks good" << std::endl;
+     }
+
+     if (userOption == 3)
+     {
+         std::cout << "Make an offer - enter the amount" << std::endl;
+     }
+
+     if (userOption == 4)
+     {
+         std::cout << "Make a bid - enter the amount" << std::endl;
+     }
+
+     if (userOption == 5)
+     {
+         std::cout << "Your wallet is empty" << std::endl;
+     }
+
+     if (userOption == 6)
+     {
+         std::cout << "Going to next time frame" << std::endl;
+     }
+}
+
 int main()
 {
     while (true)
     {
-        // 1 print help
-        std::cout << "1: Print Help " << std::endl;
+        printMenu();
+        int userOption = getUserOption();
+        processUserOption(userOption);
 
-        // 2 print exchange stats
-        std::cout << "2: Print exchange stats " << std::endl;
-
-        // 3 make an offer
-        std::cout << "3: Make an ask " << std::endl;
-
-        // 4 make a bid
-        std::cout << "4: Make a bid" << std::endl;
-
-        // 5 print wallet
-        std::cout << "5: Print Wallet" << std::endl;
-
-        // 6 continue
-        std::cout << "6: Continue " << std::endl;
-
-        std::cout << "==============================" << std::endl;
-        std::cout << "Type in 1 - 6" << std::endl;
-
-        int userOption;
-        std::cin >> userOption;
-        std::cout << "You chose " << userOption << std::endl;
-
-        if (userOption == 0)
-        {
-            std::cout << "Invalid Choice. Choose 1-6" << std::endl;
-        }
-
-        if (userOption == 1)
-        {
-            std::cout << "Your aim is to make money, analyse the market and make bids" << std::endl;
-        }
-
-        if (userOption == 2)
-        {
-            std::cout << "Market looks good" << std::endl;
-        }
-
-        if (userOption == 3)
-        {
-            std::cout << "Make an offer - enter the amount" << std::endl;
-        }
-
-        if (userOption == 4)
-        {
-            std::cout << "Make a bid - enter the amount" << std::endl;
-        }
-
-        if (userOption == 5)
-        {
-            std::cout << "Your wallet is empty" << std::endl;
-        }
-
-        if (userOption == 6)
-        {
-            std::cout << "Going to next time frame" << std::endl;
-        }
     }
         return 0;
-
 
 }
