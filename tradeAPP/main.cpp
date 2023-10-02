7bcf747403b82809ae72eff116b9852773950a2d
daianeklein <daiane.klein22@gmail.com>
2023-10-01 21:17:47 -0300

    working with classes

--- tradeAPP/main.cpp
+++ tradeAPP/main.cpp
@@ -8,6 +8,28 @@ enum class OrderBookType{bid, ask};
 class OrderBookEntry
 {
     public:
+
+        OrderBookEntry( double _price,
+                        double _amount,
+                        std::string _timestamp,
+                        std::string _product,
+                        OrderBookType _orderType)
+
+        : price(_price),
+          amount(_amount),
+          timestamp(_timestamp),
+          product(_product),
+          orderType(_orderType)
+    {
+        //this->price = price;
+        // price = _price;
+        // amount = _amount;
+        // timestamp = _timestamp;
+        // product = _product;
+        // orderType = _orderType;
+
+    }
+
     double price;
     double amount;
     std::string timestamp;
@@ -98,12 +120,16 @@ int main()
     // }
     // return 0;
 
-    OrderBookEntry order1;
-    order1.price = 10000;
-    order1.amount = 0.002;
-    order1.timestamp = "2020/03/17 17:01:24.884492";
-    order1.product = "BTC/USDT";
-    order1.orderType = OrderBookType::bid;
+    OrderBookEntry order1{1000,
+                         0.0002,
+                         "2020/03/17 17:01:24.884492",
+                         "BTC/USDT",
+                         OrderBookType::bid};
+    // order1.price = 10000;
+    // order1.amount = 0.002;
+    // order1.timestamp = "2020/03/17 17:01:24.884492";
+    // order1.product = "BTC/USDT";
+    // order1.orderType = OrderBookType::bid;
 
     std::cout << "The price is " << order1.price << std::endl;
 }
