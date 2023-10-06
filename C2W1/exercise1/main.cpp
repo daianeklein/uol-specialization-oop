82e00aad7aec1745d99edc5444f91d0740579611
daianeklein <daiane.klein22@gmail.com>
2023-10-06 13:21:40 -0300

    add main file

--- C2W1/exercise1/main.cpp
+++ C2W1/exercise1/main.cpp
@@ -32,6 +32,42 @@ class OrderBookEntry //name of the class
     OrderBookType orderType;
 };
 
+// menu 
+void printHelp() {
+    std::cout << "Your aim is to make money, analyse the market and make bids" << std::endl;
+}
+
+void printMarketStats() {
+    std::cout << "Market looks good" << std::endl;
+}
+
+void enterOffer() {
+    std::cout << "Make an offer - enter the amount" << std::endl;
+}
+
+void enterBid() {
+    std::cout << "Make a bid - enter the amount" << std::endl;
+}
+
+void printWallet() {
+    std::cout << "Your wallet is empty" << std::endl;
+}
+
+void goToNextTimeFrame() {
+    std::cout << "Going to next time frame" << std::endl;
+}
+
+void printMenu() {
+    std::cout << "1: Print Help " << std::endl;
+    std::cout << "2: Print exchange stats " << std::endl;
+    std::cout << "3: Make an ask " << std::endl;
+    std::cout << "4: Make a bid" << std::endl;
+    std::cout << "5: Print Wallet" << std::endl;
+    std::cout << "6: Continue " << std::endl;
+    std::cout << "Type in 1 - 6" << std::endl;
+}
+
+// price calculation functions
 double computeAveragePrice(std::vector<OrderBookEntry>& entries)
 {
     double sum_elements = 0.0;
@@ -83,6 +119,8 @@ double computePriceSpread(const std::vector<OrderBookEntry>& entries)
     return high_price - low_price;
 }
 
+
+// main function
 int main() 
 {
     std::vector<OrderBookEntry> entries;
